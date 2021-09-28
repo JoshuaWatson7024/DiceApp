bc5c037b345d4a460150ba84284f29d491d1cac1
Josh Watson <joshua@udel.edu>
2021-09-28 19:39:25 -0400

    Fixed timing with table updating

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,36 @@ import './App.css';
 import {DataTable} from './Components/DataTable'
 import {DiceTable} from './Components/DiceTable'
 import {Dice} from './classes/dice'
-import { getRandomNumber } from "./Utilities/Helpers";
+import { getRandomNumber, delay } from "./Utilities/Helpers";
 
 function App() {
   const[myDice, setMyDice] = useState<Dice>(new Dice([0,0]));
+  const[s, setS] = useState<boolean>(false);
   // In order from 2's to 12's
   const[dataForTable, setDataForTable] = useState<Array<number>>([0,0,0,0,0,0,0,0,0,0,0]);
 
-  function roll(): void{
+  async function roll(): Promise<void>{
+    // Know the future
     let val1: number = getRandomNumber(1,6);
     let val2: number = getRandomNumber(1,6);
-    let sum: number = val1 + val2;
-    let tempData: number[] = dataForTable;
-
-    tempData[sum-2] = tempData[sum-2] + 1;
-    setDataForTable(tempData);
+    let sum: number = val1+val2;
 
     //animation
     let lenAnimation = getRandomNumber(8,10);
-    for (let i = 0; i<lenAnimation; i++){
-      setTimeout(() => {setMyDice(new Dice(myDice.getNewVals()));;}, 100*(i^1.9));
+    for (let i = 0; i<lenAnimation-1; i++){
+      await delay(100);\
+      setMyDice(new Dice(myDice.getNewVals()));
     }
-    //make sure I end on the predestined values
-    setTimeout(() => {setMyDice(new Dice([val1,val2]));;}, 100*((lenAnimation+1)^1.9));
+    // End on predestined value
+    await delay(100);
+    setMyDice(new Dice([val1, val2]));
+
+    // Update data
+    let tempData: number[] = dataForTable;
+    tempData[sum-2] = tempData[sum-2] + 1;
+    setDataForTable(tempData);
+    //this makes it so my table updates
+    setS(!s);
   }
   
 
--- src/Utilities/Helpers.tsx
+++ src/Utilities/Helpers.tsx
@@ -4,5 +4,10 @@
  * @returns Random whole number from lower to upper (inclusive)
  */
  export function getRandomNumber(lower: number, upper: number): number {
-    return Math.floor(Math.random()*upper + lower);
+    return Math.floor(Math.random()*(upper-lower) + lower);
+  }
+
+  //https://stackoverflow.com/questions/51633328/how-to-pause-a-function-in-typescript
+  export function delay(timeInMillis: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(() => resolve(), timeInMillis));
   }
