1a7ad642e08585bf1f84ffadd5f2e4cabd60ffcd
Josh Watson <joshua@udel.edu>
2021-09-28 17:43:41 -0400

    Images of dice are displayed based on values

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import {DiceTable} from './Components/DiceTable'
 import {Dice} from './interfaces/dice'
-import dice1 from './Assets/dice1.png'
-import dice2 from './Assets/dice2.png'
-import dice3 from './Assets/dice3.png'
-import dice4 from './Assets/dice4.png'
-import dice5 from './Assets/dice5.png'
-import dice6 from './Assets/dice6.png'
 
 function App() {
   const[myDice, setMyDice] = useState<Dice>(new Dice([0,0]));
@@ -20,7 +14,6 @@ function App() {
     <div className="App">
       <button onClick={() => roll()}>Roll Dice</button>
       <DiceTable data-testId = "Dice" myDice = {myDice}></DiceTable>
-      <img src={dice1} alt="1"></img>
     </div>
   );
 }
--- src/Components/DiceTable.tsx
+++ src/Components/DiceTable.tsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
 import {Dice} from '../interfaces/dice';
+import dice1 from '../Assets/dice1.png'
+import dice2 from '../Assets/dice2.png'
+import dice3 from '../Assets/dice3.png'
+import dice4 from '../Assets/dice4.png'
+import dice5 from '../Assets/dice5.png'
+import dice6 from '../Assets/dice6.png'
 
 export const DiceTable = ({myDice}:
     {myDice: Dice}
 ): JSX.Element => {
+
+    let image_locations: string[] = [];
+    for (let i = 0; i < myDice.diceArr.length; i++){
+        image_locations[i] = (
+            myDice.diceArr[i].val == 1? dice1:
+            myDice.diceArr[i].val == 2? dice2:
+            myDice.diceArr[i].val == 3? dice3:
+            myDice.diceArr[i].val == 4? dice4:
+            myDice.diceArr[i].val == 5? dice5:
+            myDice.diceArr[i].val == 6? dice6:
+            ""
+        );
+    }
+
     return(
         <div>
-            {myDice.diceArr[0].val}
-            {myDice.diceArr[1].val}
+            <img src={image_locations[0]}></img>
+            <img src={image_locations[1]}></img>
         </div>
     );
 }
