851f8bdd3c5a274965d11a9ba367c45554a19557
Josh Watson <joshua@udel.edu>
2021-09-28 14:01:38 -0400

    Removed uneccesary vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import {DiceTable} from './Components/DiceTable'
-import {Die, Dice} from './interfaces/dice'
+import {Dice} from './interfaces/dice'
 
 function App() {
-  const[d1, setd1] = useState<Die>({val:1});
-  const[d2, setd2] = useState<Die>({val:0});
   const[myDice, setMyDice] = useState<Dice>(new Dice([0,0]));
 
   return (
