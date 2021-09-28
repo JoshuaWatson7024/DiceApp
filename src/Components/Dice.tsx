49aaa94b16928dc1651e8b9746a56a8438001154
Josh Watson <joshua@udel.edu>
2021-09-28 12:50:50 -0400

    made dice component

---
+++ src/Components/Dice.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export const Dice = ({val}:
+    {val: number}
+): JSX.Element => {
+    return(
+        <div>
+            number
+        </div>
+    );
+}
+
